d49b37b1d2dcc6fcdfaa1a6f6e25a8ddb7364a4c
Andrea Camila <112008300+aacostas25@users.noreply.github.com>
2025-06-22 11:29:14 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,6 @@ import streamlit as st
 from PIL import Image
 import os
 import numpy as np
-#import tensorflow as tf
-#from tensorflow.keras.preprocessing.image import img_to_array
 import gzip
 import pickle
 
